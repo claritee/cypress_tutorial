4a628ad05b7c8832d2ac47ef1ff499855084ae79
Claire Tran <ctran@expert360.com>
2019-04-24 18:49:33 +1000

    Adding views

--- index.js
+++ index.js
@@ -9,6 +9,13 @@ app.get('/api/authors', (req, res) => {
   Author.findAll().then(authors => res.json(authors));
 });
 
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+app.get('/', function (req, res) {
+  res.render('index', { title: 'Hey', message: 'Hello there!' })
+})
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`Running on http://localhost:${port}`);
