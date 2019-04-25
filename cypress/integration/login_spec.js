e09a10d6b5d6ac41a40529e31e86a0bf7669a117
Claire Tran <c.tran83@gmail.com>
2019-04-25 18:39:15 +1000

    Adding an assertion

--- cypress/integration/login_spec.js
+++ cypress/integration/login_spec.js
@@ -2,5 +2,6 @@ describe('End to End Tests', function() {
   it('Logs in', function() {
     cy.visit('/login', { timeout: 5000 });
     cy.get('button').click({ position: 'topLeft' });
+    cy.get('li.book').should('have.length', 3);
   })
 })
