09c101aa7fdbd3471a85608d70ada0ad57b386c1
Claire Tran <c.tran83@gmail.com>
2019-04-25 19:48:02 +1000

    Adding changes to use data tags

--- cypress/integration/login_spec.js
+++ cypress/integration/login_spec.js
@@ -1,7 +1,7 @@
 describe('End to End Tests', function() {
   it('Logs in', function() {
     cy.visit('/login', { timeout: 5000 });
-    cy.get('button').click({ position: 'topLeft' });
+    cy.get('[data-cy=login]').click({ position: 'topLeft' });
     cy.get('li.book').should('have.length', 3);
   })
 })
