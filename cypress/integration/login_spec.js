95b9d1721c87fa966991a30b267208527942bc50
Claire Tran <c.tran83@gmail.com>
2019-05-05 20:00:31 +1000

    Introducing commands

--- cypress/integration/login_spec.js
+++ cypress/integration/login_spec.js
@@ -1,7 +1,6 @@
 describe('End to End Tests', function() {
   it('Logs in', function() {
-    cy.visit('/login', { timeout: 5000 });
-    cy.get('[data-cy=login]').click({ position: 'topLeft' });
+    cy.login('foo', 'bar');
     cy.get('li.book').should('have.length', 3);
   });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -8,18 +8,13 @@
 // https://on.cypress.io/custom-commands
 // ***********************************************
 //
-//
-// -- This is a parent command --
-// Cypress.Commands.add("login", (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This is will overwrite an existing command --
-// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('login', (username, password) => {
+  // go to login page
+  cy.visit('/login', { timeout: 10000 });
+
+  // enter required fields and submit
+  cy.get('input[name="username"]').type(username);
+  cy.get('input[name="password"]').type(password);
+  cy.get('[data-cy=login]').click({ position: 'topLeft' });
+});
