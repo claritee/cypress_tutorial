753e205fccdbfdae4977b5e4cafc634bdf723b86
Claire Tran <c.tran83@gmail.com>
2019-05-05 20:29:27 +1000

    Re-using custom command

--- cypress/integration/login_spec.js
+++ cypress/integration/login_spec.js
@@ -1,6 +1,11 @@
-describe('End to End Tests', function() {
+describe('Login Tests', function() {
   it('Logs in', function() {
     cy.login('foo', 'bar');
     cy.get('li.book').should('have.length', 3);
+  });
+
+  it('unable to login with incorrect credentials', function() {
+    cy.login('xxx', 'yyy');
+    cy.get('li.book').should('have.length', 0);
   });
 });
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const { Author, Book } = require('./sequelize');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/api/authors', (req, res) => {
   Author.findAll().then(authors => res.json(authors));
@@ -17,15 +18,21 @@ app.set('view engine', 'pug');
 app.set('views', './views');
 
 app.get('/', function (req, res) {
-  res.render('index', { title: 'Hey', message: 'Hello there!' })
+  res.render('index', { title: 'Hey', message: 'Hello there!' });
 });
 
 app.get('/login', function (req, res) {
-  res.render('login', { title: 'Login', message: 'Click to login' })
+  res.render('login', { title: 'Login', message: 'Enter a username and password' });
 });
 
 app.post('/login', function (req, res) {
-  res.redirect('/books');
+  username = req.body.username;
+  password = req.body.password;
+  if (username == 'foo' && password == 'bar') {
+    res.redirect('/books');
+  } else {
+    res.render('login', { title: 'Login', message: 'Username or password is invalid' });
+  }
 });
 
 app.get('/books', function (req, res) {
